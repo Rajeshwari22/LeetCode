20e47ada6fca714963663fa24cbf6a2ab6157167
Rajeshwari Somapur <57301404+Rajeshwari22@users.noreply.github.com>
2023-03-30 13:02:54 -0700

    Delete TwoSum.py

--- TwoSum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashMap={}
-        for index,val in enumerate(nums):
-            difference=target-val
-            if difference in hashMap:
-                return [hashMap[difference],index]
-            hashMap[val]=index

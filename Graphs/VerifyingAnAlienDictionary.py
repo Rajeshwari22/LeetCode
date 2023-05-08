8ff49e97e95b6c90488148ba6a79d194a717fbce
Rajeshwari Somapur <57301404+Rajeshwari22@users.noreply.github.com>
2023-05-08 12:23:40 -0700

    Create VerifyingAnAlienDictionary.py

---
+++ Graphs/VerifyingAnAlienDictionary.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        #first differeing char
+        #if wordA is prefix of wordB then wordB must be after wordA
+        ordInd={c:i for i,c in enumerate(order)}
+        for i in range(len(words)-1):
+            w1,w2=words[i],words[i+1]
+            for j in range(len(w1)):
+                if j==len(w2):
+                    return False
+                if w1[j]!=w2[j]:
+                    if ordInd[w2[j]]<ordInd[w1[j]]:
+                        return False
+                    break
+        return True
